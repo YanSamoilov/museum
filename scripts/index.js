073ed3d4bc472f0bfaf6b6423fe03b72459f7b6b
Yan Samoilov <janchik31@yandex.ru>
2021-07-25 11:06:18 +0300

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,10 @@ const countDaysText = document.querySelector('.intro__text-days');
 const beforeActionText = document.querySelector('.intro__before-action-text');
 const anchorLinks = document.querySelector('.header__nav-list-link');
 const dateIvent = new Date(2022, 03, 18);
-let dateNow = new Date();
+const dateNow = new Date();
 
 //Функция открытия и закрытия меню с навигацией в шапке.
-const openAndCloseMenu = () => {
+const handleSwitchMobileMenu = () => {
   menu.classList.toggle('menu_visible-active');
 }
 
@@ -19,24 +19,30 @@ const calculateDays = (dateIvent, dateNow) => {
 // Построение фразы до акции с учетом склонения слов от значения числа.
 const printCount = () => {
   const countDays = calculateDays(dateIvent, dateNow);
-  let countDaysForSwitch = countDays;
-  if (countDays >= 10)
-    countDaysForSwitch = countDays % 10;
-  switch (true) {
-    case ((countDaysForSwitch === 0) || (countDaysForSwitch >= 5 && countDaysForSwitch <= 9)) :
-      beforeActionText.textContent = 'До начала акции осталось:';
-      countDaysText.textContent = countDays + ' дней';
-      break;
-    case countDaysForSwitch >= 2 && countDaysForSwitch <= 4 :
-      beforeActionText.textContent = 'До начала акции осталось:';
-      countDaysText.textContent = countDays + ' дня';
-      break;
-    case countDaysForSwitch === 1 :
-      beforeActionText.textContent = 'До начала акции остался:';
-      countDaysText.textContent = countDays + ' день';
-      break;
+  if(countDays > 0) {
+    let countDaysForSwitch = countDays;
+    if (countDays >= 10)
+      countDaysForSwitch = countDays % 10;
+    switch (true) {
+      case ((countDaysForSwitch === 0) || (countDaysForSwitch >= 5 && countDaysForSwitch <= 9)) :
+        beforeActionText.textContent = 'До начала акции осталось:';
+        countDaysText.textContent = countDays + ' дней';
+        break;
+      case countDaysForSwitch >= 2 && countDaysForSwitch <= 4 :
+        beforeActionText.textContent = 'До начала акции осталось:';
+        countDaysText.textContent = countDays + ' дня';
+        break;
+      case countDaysForSwitch === 1 :
+        beforeActionText.textContent = 'До начала акции остался:';
+        countDaysText.textContent = countDays + ' день';
+        break;
+    }
+  } else {
+    beforeActionText.textContent = 'Акция началась';
+    countDaysText.textContent ='';
   }
+
 }
 
-buttonOpenMenu.addEventListener('click', openAndCloseMenu);
 printCount();
+buttonOpenMenu.addEventListener('click', handleSwitchMobileMenu);
